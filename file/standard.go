d8057377d886c5d544b7b5c54f33bda37683903a
pguma <p.guma.service+git@gmail.com>
2023-08-26 17:08:39 +0900

    add file access pattern with standard package

--- file/standard.go
+++ file/standard.go
@@ -1,9 +1,47 @@
 package file_standard
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"os"
 )
 
 func ReadFile(fp string) ([]byte, error) {
+	// 事前にFileサイズを検証してその通りにバッファサイズを確保してから読み込むのでReadAllより高速な模様
 	return os.ReadFile(fp)
+}
+
+func ReadFileAll(fp string) ([]byte, error) {
+	f, err := os.OpenFile(fp, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		fmt.Printf("error %v", err)
+		return nil, err
+	}
+
+	defer f.Close()
+
+	return io.ReadAll(f)
+}
+
+func ReadFileLineByLine(fp string) ([]byte, error) {
+	f, err := os.Open(fp)
+	if err != nil {
+		fmt.Printf("error %v", err)
+		return nil, err
+	}
+
+	defer f.Close()
+
+	bu := bufio.NewReaderSize(f, 1024)
+
+	var b = []byte{}
+	for {
+		line, _, err := bu.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		b = append(b, line...)
+	}
+	return b, nil
 }

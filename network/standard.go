cd25877ebf15672852a4c2d0d92a79898106a6ba
pguma <p.guma.service+git@gmail.com>
2023-08-27 15:19:54 +0900

    add netwoek access sample with response json

--- network/standard.go
+++ network/standard.go
@@ -1,6 +1,7 @@
 package network_standard
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -20,4 +21,37 @@ func Get(loc string) string {
 	}
 
 	return string(s)
+}
+
+type JsonResponse interface{}
+type SampleResponse struct {
+	Name string
+	Mail string
+	Tel  string
+}
+
+func (sr *SampleResponse) Str() string {
+	return sr.Name + "|" + sr.Mail + "|" + sr.Tel
+}
+
+func JsonGet(loc string) []SampleResponse {
+	re, err := http.Get(loc)
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+
+	defer re.Body.Close()
+
+	s, err := io.ReadAll(re.Body)
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+
+	var data []SampleResponse
+	err = json.Unmarshal(s, &data)
+	if err != nil {
+		fmt.Printf("json parse error. error detail=%v", err)
+	}
+
+	return data
 }
--- network/standard_test.go
+++ network/standard_test.go
@@ -1,6 +1,8 @@
 package network_standard
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGet(t *testing.T) {
 	resb := Get("https://www.google.com/")
@@ -8,4 +10,13 @@ func TestGet(t *testing.T) {
 		t.Errorf("can not get contents.")
 	}
 	t.Logf("contents: %s", resb)
+}
+
+func TestJsonGet(t *testing.T) {
+	resj := JsonGet("https://tuyano-dummy-data.firebaseio.com/mydata.json")
+
+	for _, c := range resj {
+		s := c.Str()
+		t.Logf("%v", s)
+	}
 }

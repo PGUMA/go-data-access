d1f56ad9c24f09de19fc7836f625c6f21f5de3eb
pguma <p.guma.service+git@gmail.com>
2023-09-07 17:32:50 +0900

    add db access sample with standard library

--- database/standard.go
+++ database/standard.go
@@ -1,5 +1,51 @@
 package database_standard
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
 	_ "github.com/lib/pq"
 )
+
+type MyData struct {
+	ID   int
+	Name string
+	Mail string
+	Age  int
+}
+
+func (m *MyData) Str() string {
+	e, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+		return "{ error }"
+	}
+	return string(e)
+}
+
+func SelectMyDataAll() {
+	// TODO 環境変数等から接続文字列を取得する
+	con, err := sql.Open("postgres", "postgres://pguma:password@localhost:5432/dev?sslmode=disable")
+	if err != nil {
+		log.Fatalln("failed to connect DB", err)
+	}
+
+	defer con.Close()
+
+	q := "select * from mydata"
+	rs, err := con.Query(q)
+	if err != nil {
+		log.Fatalln("query error", err)
+	}
+
+	for rs.Next() {
+		var md MyData
+		err := rs.Scan(&md.ID, &md.Name, &md.Mail, &md.Age)
+		if err != nil {
+			log.Fatalln("row fetch error", err)
+		}
+		fmt.Println(md.Str())
+	}
+}
---
+++ database/standard_test.go
@@ -0,0 +1,7 @@
+package database_standard
+
+import "testing"
+
+func TestSelectMyDataAll(t *testing.T) {
+	SelectMyDataAll()
+}
